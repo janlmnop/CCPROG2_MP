4319a0e48ad6ddf34553a635a01331c2484c8198
janlmnop <janelledaguiso@gmail.com>
2025-03-17 00:07:04 +0800

    mar16_update

--- definitions.h
+++ definitions.h
@@ -2,30 +2,35 @@
 #include <strings.h>
 
 
-// DEFINITIONS
-#define MAX_USERS 50
-#define MAX_BATTLEPETS 60
+#define MAX_USERS 50            // Maximum number of Users / Players
+#define MAX_BATTLEPETS 60       // Maximum number of BattlePets
 
-typedef char String36[37];      //Player username
-typedef char String30[31];      //BattlePet name and elemental affinity
-typedef char String240[241];    //BattlePet description
+typedef char String36[37];      // Player's username
+typedef char String30[31];      // BattlePet name and elemental affinity
+typedef char String240[241];    // BattlePet description
 
-// STRUCTURES
+
+/**
+ * Represents a BattlePet 
+ */
 struct BattlePets
 {
-    String30 battlepet;         //BattlePet's name
-    String30 elementalaffinity; //BattlePet's elemental affinity
-    String240 description;      //BattlePet short description
-    int matchcount;             //number of times the BattlePet has appeared in matches
+    String30 battlepet;         // BattlePet's name
+    String30 elementalaffinity; // BattlePet's elemental affinity
+    String240 description;      // BattlePet's short description
+    int matchcount;             // Number of times the BattlePet has appeared in matches
 };
 
+/**
+ * Represents a Player
+ */
 struct PlayerTag
 {
-    String36 playername;            //player username
-    int wincount;                   //player win count
-    int losscount;                  //player loss count
-    int drawcount;                  //player draw count
-    struct BattlePets roster[3][3]; //array of arrays
+    String36 playername;            // Player's username
+    int wincount;                   // Player's win count
+    int losscount;                  // Player's loss count
+    int drawcount;                  // Player's draw count
+    struct BattlePets roster[3][3]; // Player's BattlePet roster
 };
 
 
@@ -36,9 +41,9 @@ int countPlayers (struct PlayerTag playerlist[]);
 void selectPlayer (struct PlayerTag playerlist[], struct PlayerTag player[]);
 void displayRoster (struct PlayerTag player[]);
 void saveRosterInfo (struct BattlePets BP[], struct PlayerTag player[]);
-void loadSavedRoster (struct BattlePets BP[], struct PlayerTag player[]);
-int checkPetAvailability (String36 BattlePet, struct PlayerTag player[]);
-int returnBPIndex (struct BattlePets BP[], String36 name);
+void loadSavedRoster (struct BattlePets BP[], struct PlayerTag currentplayer[], struct PlayerTag otherplayer[]);
+int checkPetAvailability (int selectedBP, struct PlayerTag player[], struct BattlePets BP[]);
+int returnBPIndex (struct BattlePets BP[], int selectedBP);
 int returnPlayerIndex (struct PlayerTag playerlist[], String36 name);
 void createNewRoster (struct BattlePets BP[], struct PlayerTag currentplayer[], struct PlayerTag otherplayer[]);
 void displayChallengers (struct PlayerTag player1[], struct PlayerTag player2[]);
@@ -47,10 +52,9 @@ void countWins (char results[3][3], int *ones, int *twos);
 void showMatchResults (struct PlayerTag playerlist[], struct BattlePets BP[], struct PlayerTag player1[], struct PlayerTag player2[]);
 
 /* selectComPetDium.c */
-void populateBattlePetList (struct BattlePets BP[], int *BPtotal);
-void viewBattlePets (struct BattlePets BP[], int *BPtotal);
-void addBattlePet (struct BattlePets BP[], int *BPtotal);
-void editBattlePet (struct BattlePets BP[]);
-void deleteBattlePet (struct BattlePets BP[], int *BPtotal);
-void displayBoard (String36 Board[][3]);
-
+// void populateBattlePetList (struct BattlePets BP[], int *BPtotal);
+// void viewBattlePets (struct BattlePets BP[], int *BPtotal);
+// void addBattlePet (struct BattlePets BP[], int *BPtotal);
+// void editBattlePet (struct BattlePets BP[]);
+// void deleteBattlePet (struct BattlePets BP[], int *BPtotal);
+// void displayBoard (String36 Board[][3]);
